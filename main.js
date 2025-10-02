41b0270b5da29c66feb0c849e041969ffb98a28a
citop <69157589+dojji@users.noreply.github.com>
2025-10-02 12:17:58 +0300

    Added index.html

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <h1>Welcome to Your Preview</h1>
+    <p>Your application is ready to view.</p>
+    <p class="info">What would you like to build?</p>
+  </div>
+`;
